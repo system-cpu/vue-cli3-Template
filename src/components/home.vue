44906f383292037c6e1af940bcfd6ab6b5e0bb3f
liyunlong <2315673456@qq.com>
2019-10-25 17:28:54 +0800

    'cishi'

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <navigation  v-bind:inshow="seal.inshow" :head_name="seal.head_name"  :right_inshow="seal.right_inshow" :right_name="seal.right_name"></navigation>
          <div @click="route('/me',{'id':1,'name':'liyunlong'})">
-            路由
+            {{name}}
          </div>
       <HelloWorld v-bind:idx="seal.idx"></HelloWorld>
   </div>
@@ -20,7 +20,8 @@ export default {
           head_name:"哈哈哈",
           right_inshow:true,
           right_name:"djhfwerf"
-      } 
+      } ,
+      name:""
     }
   },
  components:{
@@ -34,19 +35,20 @@ export default {
     }
   },
   created(){
-    // console.log(window.g.baseUrl)
-    //  var that = this;
-    //   that.$axios({
-    //     method: "post",
-    //     url: window.g.baseUrl + "api/Branch/branch_honor",
-    //     params: {
-    //       shop_id:3,
-    //       p:that.current,
-    //       count:9
-    //     }
-    //   }).then((res)=> {
-    //     console.log(res.data);
-    //   });
+    console.log(window.g.baseUrl)
+     var that = this;
+      that.$axios({
+        method: "post",
+        url: window.g.baseUrl + "api/Branch/branch_honor",
+        params: { 
+          shop_id:3,
+          p:that.current,
+          count:9
+        }
+      }).then((res)=> {
+        console.log(res.data.data.cat_name);
+        that.name=res.data.data.cat_name
+      });
   }
 }
 </script>
